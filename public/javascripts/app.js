b2aacf354a491a4cfb3c87017930f9b206cb633e
Andres Rodriguez <andres@joyent.com>
2011-03-08 17:00:07 -0800

    - Close notifications after 5 seconds - Don't show notifications from yourself

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -91,9 +91,10 @@ function renderAnnouncement(message){
 
 function displayNotification(message) {
   if (window.webkitNotifications.checkPermission() == 0) {
-    if (message.body.indexOf(nick) != -1) {
+    if (message.body.indexOf(nick) != -1 && message.from.indexOf(nick) == -1) {
       var n = window.webkitNotifications.createNotification('', message.from, message.body);
       n.show();
+      setTimeout(function(){n.cancel()}, 5000);
     }
 	}
 }
